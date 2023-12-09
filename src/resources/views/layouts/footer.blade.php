2494c7aa9199686f16cdabc40f89c11332940edf
kai <sea.k.naka@gmail.com>
2023-12-09 23:44:14 +0900

    フッター完成

---
+++ src/resources/views/layouts/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="bg-white dark:bg-gray-900">
+    <div class="container flex flex-col items-center justify-between p-6 mx-auto space-y-4 sm:space-y-0 sm:flex-row">
+        <a href="#">
+            <img class="w-auto h-7" src="https://merakiui.com/images/full-logo.svg" alt="">
+        </a>
+
+        <p class="text-sm text-gray-600 dark:text-gray-300">© Copyright 2023. All Rights Reserved.</p>
+    </div>
+</footer>
--- src/resources/views/layouts/header.blade.php
+++ src/resources/views/layouts/header.blade.php
@@ -23,7 +23,7 @@
                     </div>
     
                     <!-- SP menu button -->
-                    <div class="flex lg:hidden">
+                    <div class="flex lg:hidden hidden">
                         <button x-cloak @click="isOpen = !isOpen" type="button" class="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400" aria-label="toggle menu">
                             <svg x-show="!isOpen" xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M4 8h16M4 16h16" />
@@ -37,10 +37,10 @@
                 </div>
     
                 <!-- SP Menu open: "block", Menu closed: "hidden" -->
-                <div x-cloak :class="[isOpen ? 'translate-x-0 opacity-100 ' : 'opacity-0 -translate-x-full']" class="absolute inset-x-0 z-20 px-6 py-2 transition-all duration-300 ease-in-out bg-white top-24 dark:bg-gray-800 lg:mt-0 lg:p-0 lg:top-0 lg:relative lg:bg-transparent lg:w-auto lg:opacity-100 lg:translate-x-0 flex flex-col lg:flex-row lg:items-center items-end">
+                <div x-cloak :class="[isOpen ? 'translate-x-0 opacity-100 ' : 'opacity-0 -translate-x-full']" class="absolute inset-x-0 z-20 px-6 py-2 transition-all duration-300 ease-in-out bg-white top-24 dark:bg-gray-800 lg:mt-0 lg:p-0 lg:top-0 lg:relative lg:bg-transparent lg:w-auto lg:opacity-100 lg:translate-x-0 flex flex-col lg:flex-row lg:items-center items-end hidden lg:flex">
     
                     <!-- SP-profile -->
-                    <button type="button" class="my-2 flex lg:hidden items-center focus:outline-none" aria-label="toggle profile dropdown">
+                    <button type="button" class="my-2 flex lg:hidden items-center focus:outline-none hidden" aria-label="toggle profile dropdown">
                         <h3 class="mx-2 text-gray-700 dark:text-gray-200 lg:hidden">Khatab wedaa</h3>
                         <div class="w-8 h-8 overflow-hidden border-2 border-gray-400 rounded-full">
                             <img src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80" class="object-cover w-full h-full" alt="avatar">
@@ -48,14 +48,14 @@
                     </button>
     
                     <!-- SP-notice -->
-                    <button class="my-2 block lg:hidden text-gray-600 transition-colors duration-300 transform dark:text-gray-200 hover:text-gray-700 dark:hover:text-gray-400 focus:text-gray-700 dark:focus:text-gray-400 focus:outline-none" aria-label="show notifications">
+                    <button class="my-2 block lg:hidden text-gray-600 transition-colors duration-300 transform dark:text-gray-200 hover:text-gray-700 dark:hover:text-gray-400 focus:text-gray-700 dark:focus:text-gray-400 focus:outline-none hidden" aria-label="show notifications">
                         <svg class="w-6 h-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M15 17H20L18.5951 15.5951C18.2141 15.2141 18 14.6973 18 14.1585V11C18 8.38757 16.3304 6.16509 14 5.34142V5C14 3.89543 13.1046 3 12 3C10.8954 3 10 3.89543 10 5V5.34142C7.66962 6.16509 6 8.38757 6 11V14.1585C6 14.6973 5.78595 15.2141 5.40493 15.5951L4 17H9M15 17V18C15 19.6569 13.6569 21 12 21C10.3431 21 9 19.6569 9 18V17M15 17H9" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                         </svg>
                     </button>
     
                     <!-- SP-cart -->
-                    <div class="my-2 flex lg:hidden justify-center">
+                    <div class="my-2 flex lg:hidden justify-center hidden">
                         <a class="relative text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-300" href="#">
                             <svg class="w-5 h-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M3 3H5L5.4 5M7 13H17L21 5H5.4M7 13L5.4 5M7 13L4.70711 15.2929C4.07714 15.9229 4.52331 17 5.41421 17H17M17 17C15.8954 17 15 17.8954 15 19C15 20.1046 15.8954 21 17 21C18.1046 21 19 20.1046 19 19C19 17.8954 18.1046 17 17 17ZM9 19C9 20.1046 8.10457 21 7 21C5.89543 21 5 20.1046 5 19C5 17.8954 5.89543 17 7 17C8.10457 17 9 17.8954 9 19Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
@@ -64,7 +64,7 @@
                         </a>
                     </div>
     
-                    <div class="flex items-end flex-col lg:flex-row lg:mx-1">
+                    <div class="items-end flex-col lg:flex-row lg:mx-1 hidden lg:flex">
                         <a class="my-2 text-sm leading-5 text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400 hover:underline lg:mx-4 lg:my-0" href="#">Home</a>
                         <a class="my-2 text-sm leading-5 text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400 hover:underline lg:mx-4 lg:my-0" href="#">Blog</a>
                         <a class="my-2 text-sm leading-5 text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400 hover:underline lg:mx-4 lg:my-0" href="#">Compoents</a>
@@ -100,7 +100,7 @@
                     </div>
     
                     <!-- Search input on SP screen -->
-                    <div class="my-4 lg:hidden w-full">
+                    <div class="my-4 lg:hidden w-full hidden">
                         <div class="relative">
                             <span class="absolute inset-y-0 left-0 flex items-center pl-3">
                                 <svg class="w-5 h-5 text-gray-400" viewBox="0 0 24 24" fill="none">
