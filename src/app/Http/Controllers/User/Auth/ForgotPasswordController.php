b015eedacd58efd5146f256de46412aa4bba7c45
kai <sea.k.naka@gmail.com>
2023-12-20 19:49:22 +0900

    独自リセットメール実装中2

--- src/app/Http/Controllers/User/Auth/ForgotPasswordController.php
+++ src/app/Http/Controllers/User/Auth/ForgotPasswordController.php
@@ -34,7 +34,7 @@ class ForgotPasswordController extends Controller
         // We will send the password reset link to this user. Once we have attempted
         // to send the link, we will examine the response then see the message we
         // need to show to the user. Finally, we'll send out a proper response.
-        $response = $this->broker()->sendResetLink(
+        $response = $this->broker('users')->sendResetLink(
             $this->credentials($request)
         );
 
