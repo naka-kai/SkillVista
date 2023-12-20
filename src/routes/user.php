b213722f9bc6033912b9e6a0c75d6af3d4f5ce3d
kai <sea.k.naka@gmail.com>
2023-12-20 09:12:19 +0900

    独自リセットメール実装中2

--- src/routes/user.php
+++ src/routes/user.php
@@ -19,10 +19,10 @@ Route::prefix('user')->name('user.')->group(function() {
     Route::get('register', [RegisterController::class, 'showRegistrationForm'])->name('showRegistrationForm');
     Route::post('register', [RegisterController::class, 'register'])->name('register');
     // パスワードリセット
-    // Route::get('password/reset', [ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
-    // Route::post('password/email', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
-    // Route::get('password/reset/{token}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset');
-    Route::get('/forgot-password', [ForgotPasswordController::class, 'forgotPassword'])->name('password.request');
+    Route::post('password/email', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
+    Route::get('password/reset', [ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
+    Route::post('password/reset', [ResetPasswordController::class, 'reset'])->name('password.update');
+    Route::get('password/reset/{token}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset');
 });
 
 // ログイン後
@@ -30,7 +30,6 @@ Route::middleware('auth:user')->group(function() {
     Route::post('/top', [TopController::class, 'top']);
 });
 Route::middleware('auth:user')->prefix('user')->name('user.')->group(function() {
-    Route::post('password/reset', [ResetPasswordController::class, 'reset'])->name('password.update');
     // ログアウト
     Route::post('logout', [LoginController::class, 'logout'])->name('logout');
     // マイコース
