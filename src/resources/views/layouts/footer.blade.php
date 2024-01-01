eeee7d8131ae5ad832e299fbb8daa4c33bdc8635
kai <sea.k.naka@gmail.com>
2024-01-01 10:07:48 +0900

    コース編集画面ルーティング

--- src/resources/views/layouts/footer.blade.php
+++ src/resources/views/layouts/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="bg-white dark:bg-gray-900">
+<footer class="mt-14 bg-white dark:bg-gray-900">
     <div class="container flex flex-col items-center justify-between p-6 mx-auto space-y-4 sm:space-y-0 sm:flex-row">
         <a href="{{ route('top') }}" class="font-bold text-[18px]">SkillVista</a>
 
