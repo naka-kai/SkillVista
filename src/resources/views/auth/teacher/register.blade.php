10a27e46e2ea884cb8ae388dcc5640b02721a522
kai <sea.k.naka@gmail.com>
2024-01-07 06:51:57 +0900

    教師登録必須項目にマーク付加

--- src/resources/views/auth/teacher/register.blade.php
+++ src/resources/views/auth/teacher/register.blade.php
@@ -15,7 +15,7 @@
                     @csrf
 
                     <div>
-                        <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">姓</label>
+                        <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">姓 *</label>
                         <input type="text" class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40  @error('last_name') is-invalid @enderror" name="last_name" value="{{ old('last_name') }}" autocomplete="last_name" autofocus />
                         @foreach ($errors->get('last_name') as $message)
                             <p class="text-sm text-red-400 mt-1">{{ $message }}</p>
@@ -23,7 +23,7 @@
                     </div>
 
                     <div>
-                        <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">名</label>
+                        <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">名 *</label>
                         <input type="text" class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40  @error('first_name') is-invalid @enderror" name="first_name" value="{{ old('first_name') }}" autocomplete="first_name" autofocus />
                         @foreach ($errors->get('first_name') as $message)
                             <p class="text-sm text-red-400 mt-1">{{ $message }}</p>
@@ -31,7 +31,7 @@
                     </div>
 
                     <div>
-                        <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">姓（英語表記）</label>
+                        <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">姓（英語表記） *</label>
                         <input type="text" class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40  @error('last_name_en') is-invalid @enderror" name="last_name_en" value="{{ old('last_name_en') }}" autocomplete="last_name_en" autofocus />
                         @foreach ($errors->get('last_name_en') as $message)
                             <p class="text-sm text-red-400 mt-1">{{ $message }}</p>
@@ -39,7 +39,7 @@
                     </div>
 
                     <div>
-                        <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">名（英語表記）</label>
+                        <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">名（英語表記） *</label>
                         <input type="text" class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40  @error('first_name_en') is-invalid @enderror" name="first_name_en" value="{{ old('first_name_en') }}" autocomplete="first_name_en" autofocus />
                         @foreach ($errors->get('first_name_en') as $message)
                             <p class="text-sm text-red-400 mt-1">{{ $message }}</p>
@@ -47,7 +47,7 @@
                     </div>
 
                     <div>
-                        <p class="block mb-2 text-sm text-gray-600 dark:text-gray-200">アイコン画像</p>
+                        <p class="block mb-2 text-sm text-gray-600 dark:text-gray-200">アイコン画像 *</p>
                         <div class="flex items-end mb-4 mt-3">
                             <img id="displayImg" src="{{ asset('img/kkrn_icon_user_13.png') }}" alt="" class="w-24 h-24 rounded-full">
                             <button class="py-1 px-3 text-sm font-medium text-center text-gray-500 bg-gray-100 border rounded-sm hover:opacity-70 ml-5" type="button" id="displayImgDelete">削除</button>
@@ -62,7 +62,7 @@
                     </div>
 
                     <div>
-                        <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">プロフィール</label>
+                        <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">プロフィール *</label>
                         <textarea type="text" class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40  @error('profile') is-invalid @enderror" name="profile" autocomplete="profile" autofocus rows="5">{{ old('profile') }}</textarea>
                         @foreach ($errors->get('profile') as $message)
                             <p class="text-sm text-red-400 mt-1">{{ $message }}</p>
@@ -70,7 +70,7 @@
                     </div>
 
                     <div>
-                        <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">メールアドレス</label>
+                        <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">メールアドレス *</label>
                         <input type="email" placeholder="example@example.com" class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40 @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" autocomplete="email" />
                         @foreach ($errors->get('email') as $message)
                             <p class="text-sm text-red-400 mt-1">{{ $message }}</p>
@@ -78,7 +78,7 @@
                     </div>
 
                     <div>
-                        <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">パスワード</label>
+                        <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">パスワード *</label>
                         <input type="password" class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40 @error('password') is-invalid @enderror" name="password" autocomplete="new-password" />
                         <div class="text-sm text-gray-600 mt-2 ml-3">
                             <p class="mb-1">パスワードは以下の基準を満たしてください</p>
@@ -95,7 +95,7 @@
                     </div>
 
                     <div>
-                        <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">パスワード再入力</label>
+                        <label class="block mb-2 text-sm text-gray-600 dark:text-gray-200">パスワード再入力 *</label>
                         <input type="password" class="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40"  id="password-confirm" name="password_confirmation" autocomplete="new-password" />
                     </div>
 
