c61e9995094f19a3eff2b8da444e62e0ead97452
kai <sea.k.naka@gmail.com>
2024-01-22 23:35:52 +0900

    削除失敗時のフラッシュメッセージ表示

--- src/app/Http/Controllers/MovieController.php
+++ src/app/Http/Controllers/MovieController.php
@@ -6,6 +6,8 @@ use App\Models\Course;
 use Illuminate\Http\Request;
 use App\Models\Movie;
 
+use Illuminate\Support\Facades\Log;
+
 class MovieController extends Controller
 {
     public function show()
@@ -15,27 +17,33 @@ class MovieController extends Controller
 
     public function destroy(Request $request, $teacherName, $courseName)
     {
-        // 渡ってきた１つのコースの情報
-        $course = Course::with([
-            'teacher', 
-            'rates.users',
-            'chapters' => function($q) {
-                $q->orderBy('display_num');
-                $q->with([
-                    'tests', 'movies'
-                ]);
-            },
-        ])
-        ->where('course_url', '=', $courseName)
-        ->first();
+        try {
+            // 渡ってきた１つのコースの情報
+            $course = Course::with([
+                'teacher', 
+                'rates.users',
+                'chapters' => function($q) {
+                    $q->orderBy('display_num');
+                    $q->with([
+                        'tests', 'movies'
+                    ]);
+                },
+            ])
+            ->where('course_url', '=', $courseName)
+            ->first();
+    
+            if ($request->input('action') === 'delete_movie') {
+                $movie_id = $request->input('movie_id');
+                $movie = Movie::findOrFail($movie_id);
+                $movie->delete();
+    
+                return redirect()->route('course.update', ['courseName' => $courseName, 'teacherName' => $teacherName])->with(['course' => $course, 'flash_message' => '動画を1つ削除しました']);
+            }
+            return redirect()->route('teacher.myCourse');
+        } catch (\Exception $e) {
+            Log::error("エラーが発生しました: " . $e);
 
-        if ($request->input('action') === 'delete_movie') {
-            $movie_id = $request->input('movie_id');
-            $movie = Movie::findOrFail($movie_id);
-            $movie->delete();
-
-            return redirect()->route('course.update', ['courseName' => $courseName, 'teacherName' => $teacherName])->with(['course' => $course, 'flash_message' => '動画を1つ削除しました']);
+            return redirect()->route('course.update', ['courseName' => $courseName, 'teacherName' => $teacherName])->with(['course' => $course, 'flash_message' => '動画の削除に失敗しました']);
         }
-        return redirect()->route('teacher.myCourse');
     }
 }
