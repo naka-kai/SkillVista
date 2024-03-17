bff2da38824c8d3959473c088b123ca498734c35
kai <sea.k.naka@gmail.com>
2024-03-17 11:34:02 +0900

    ユーザー欲しい物リスト実装

--- src/app/Http/Controllers/User/UserController.php
+++ src/app/Http/Controllers/User/UserController.php
@@ -37,8 +37,31 @@ class UserController extends Controller
         return view('Pages.User.my_course', compact('courses', 'userName', 'keyword'));
     }
 
-    public function wishList()
+    public function wishList(Request $request, $userName)
     {
-        return view('Pages.User.wish_list');
+    $user_id = Auth::guard('user')->id();
+
+        // 自分のお気に入りコースのみ表示
+        $courses = Course::with(['teacher', 'rates'])
+            ->leftJoin('course_user', 'courses.id', '=', 'course_user.course_id')
+            ->where('user_id', '=', $user_id)
+            ->where('status', '=', '1')
+            ->get();
+        
+        // お気に入りコースに登録されている「タイトル」・「コースの軽い説明」の文字列から検索処理
+        $keyword = $request->query('keyword');
+        if(!empty($keyword)) {
+            $courses = Course::with(['teacher', 'rates'])
+                ->leftJoin('course_user', 'courses.id', '=', 'course_user.course_id')
+                ->where('user_id', '=', $user_id)
+                ->where('status', '=', '1')
+                ->where(function ($query) use ($keyword) {
+                    $query->where('title', 'LIKE', "%{$keyword}%")
+                        ->orWhere('description', 'LIKE', "%{$keyword}%");
+                })
+                ->paginate(10);
+        }
+        
+        return view('Pages.User.wish_list', compact('courses', 'userName', 'keyword'));
     }
 }
