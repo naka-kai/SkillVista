cec5e0fa3223e176f6e6797d74027d0499efa5c6
kai <sea.k.naka@gmail.com>
2024-03-17 10:36:11 +0900

    ユーザーマイコース検索

--- src/app/Http/Controllers/User/UserController.php
+++ src/app/Http/Controllers/User/UserController.php
@@ -4,12 +4,37 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Course;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    public function myCourse()
+    public function myCourse(Request $request, $userName)
     {
-        return view('Pages.User.my_course');
+        $user_id = Auth::guard('user')->id();
+
+        // 自分の購入済みコース（マイコース）のみ表示
+        $courses = Course::with(['teacher', 'rates'])
+            ->leftJoin('course_user', 'courses.id', '=', 'course_user.course_id')
+            ->where('user_id', '=', $user_id)
+            ->where('status', '=', '3')
+            ->get();
+        
+        // マイコースに登録されている「タイトル」・「コースの軽い説明」の文字列から検索処理
+        $keyword = $request->query('keyword');
+        if(!empty($keyword)) {
+            $courses = Course::with(['teacher', 'rates'])
+                ->leftJoin('course_user', 'courses.id', '=', 'course_user.course_id')
+                ->where('user_id', '=', $user_id)
+                ->where('status', '=', '3')
+                ->where(function ($query) use ($keyword) {
+                    $query->where('title', 'LIKE', "%{$keyword}%")
+                        ->orWhere('description', 'LIKE', "%{$keyword}%");
+                })
+                ->paginate(10);
+        }
+
+        return view('Pages.User.my_course', compact('courses', 'userName', 'keyword'));
     }
 
     public function wishList()
--- src/resources/views/Pages/User/my_course.blade.php
+++ src/resources/views/Pages/User/my_course.blade.php
@@ -1,48 +1,108 @@
 @extends('Layouts.app')
 
+@section('style')
+    <link rel="stylesheet" href="{{ asset('css/rate.css') }}">
+@endsection
+
 @section('content')
-<section class="bg-white dark:bg-gray-900">
-    <div class="py-10">
-        <div class="flex items-center justify-between">
-            <h1 class="text-2xl font-semibold text-gray-800 capitalize lg:text-3xl dark:text-white">マイコース</h1>
-        </div>
+    <div class="flex flex-col">
+        <div class="bg-white dark:bg-gray-900">
+            <div class="py-10">
+                <div class="flex items-center justify-between">
+                    <div class="flex items-center">
+                        <h1 class="text-2xl font-semibold text-gray-800 capitalize lg:text-3xl dark:text-white">マイコース</h1>
 
-        <hr class="my-8 border-gray-200 dark:border-gray-700">
+                        <!-- 検索 -->
+                        <form action="{{ route('user.myCourse', ['userName' => $userName]) }}" method="GET">
+                            <div class="my-4 mx-4">
+                                <div class="relative">
+                                    <button type="submit" value="search"
+                                        class="absolute inset-y-0 left-0 flex items-center pl-3">
+                                        <svg class="w-5 h-5 text-gray-400" viewBox="0 0 24 24" fill="none">
+                                            <path
+                                                d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
+                                                stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                                stroke-linejoin="round"></path>
+                                        </svg>
+                                    </button>
 
-        <div class="grid grid-cols-1 gap-8 md:grid-cols-2 xl:grid-cols-3">
-            @for ($i = 0; $i < 10; $i++)
-                <a href="{{ route('course', ['courseName' => 'courseName']) }}" class="hover:opacity-70">
-                    <div class="my-5">
-                        <img class="object-cover object-center w-full h-64 rounded-lg lg:h-80" src="https://images.unsplash.com/photo-1624996379697-f01d168b1a52?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="">
-
-                        <div class="mt-8">
-                            <h1 class="mt-4 text-xl font-semibold text-gray-800 dark:text-white">
-                                現役シリコンバレーエンジニアが教えるアルゴリズム・データ構造・コーディングテスト入門
-                            </h1>
-
-                            <p class="mt-2">前半はアルゴリズムとデータ構造の基礎をPythonを用いて習得し、後半にはコーディング面接対策も行います。</p>
-
-                            <p class="mt-4 text-sm">山田 太郎</p>
-
-                            <div class="flex items-center justify-between mt-4">
-                                <div class="flex items-center">
-                                    <p class="font-bold mr-1">5.0</p>
-                                    <p class="text-lg font-medium text-gray-700 dark:text-gray-300 hover:underline hover:text-gray-500">★★★★★</p>
-
-                                    <p class="text-sm text-gray-500 dark:text-gray-400 ml-1">（23,415）</p>
+                                    <input type="text" name="keyword"
+                                        class="w-full py-2 pl-10 pr-4 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 focus:ring-blue-300"
+                                        placeholder="Search">
                                 </div>
                             </div>
-
-                            {{-- <div class="bg-yellow-200 py-2 px-4 w-36 mt-2">
-                                <p class="text-sm text-center">ベストセラー</p>
-                            </div> --}}
-
-                        </div>
+                        </form>
                     </div>
-                </a>
-            @endfor
+                </div>
 
+                @if (isset($keyword))
+                    <h1 class="text-2xl font-semibold text-gray-800 capitalize lg:text-3xl dark:text-white">
+                        「{{ $keyword }}」の検索結果</h1>
+                @endif
+
+                <hr class="my-8 border-gray-200 dark:border-gray-700">
+
+                <div class="grid grid-cols-1 gap-8 md:grid-cols-2 xl:grid-cols-3">
+                    @foreach ($courses as $course)
+                        @php
+                            // 評価を計算
+                            $rates = $course->rates;
+                            $total_rating = 0;
+
+                            foreach ($rates as $rate) {
+                                $total_rating += $rate->rate;
+                            }
+                            $rated_people_num = count($rates);
+
+                            if ($rated_people_num) {
+                                $rate_avg = $total_rating / $rated_people_num;
+                                $rate = sprintf('%.1f', $rate_avg);
+                            } else {
+                                $rate = 0;
+                            }
+                        @endphp
+                        <a href="{{ route('course', ['courseName' => $course->course_url]) }}" class="hover:opacity-70">
+                            <div class="my-5">
+                                <img class="object-cover object-center w-full h-64 rounded-lg lg:h-80"
+                                    src="https://images.unsplash.com/photo-1624996379697-f01d168b1a52?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+                                    alt="">
+
+                                <div class="mt-8">
+                                    <h1 class="mt-4 text-xl font-semibold text-gray-800 dark:text-white">
+                                        {{ $course->title }}
+                                    </h1>
+
+                                    <p class="mt-2">{{ $course->description }}</p>
+
+                                    <p class="mt-4 text-sm">{{ $course->teacher->last_name }}
+                                        {{ $course->teacher->first_name }}</p>
+
+                                    <div class="flex items-center justify-between mt-4">
+                                        <div class="flex items-center">
+                                            <p class="font-bold mr-1">
+                                                {{ $rate }}
+                                            </p>
+                                            <p
+                                                class="text-lg font-medium text-gray-700 dark:text-gray-300 hover:underline hover:text-gray-500">
+                                                <span class="star5_rating" data-rate="{{ $rate }}"></span>
+                                            </p>
+
+                                            <p class="text-sm text-gray-500 dark:text-gray-400 ml-1">
+                                                （{{ $rated_people_num }}）</p>
+                                        </div>
+                                    </div>
+
+                                    {{-- <div class="bg-yellow-200 py-2 px-4 w-36 mt-2">
+                                    <p class="text-sm text-center">ベストセラー</p>
+                                </div> --}}
+
+                                </div>
+                            </div>
+                        </a>
+                    @endforeach
+
+                </div>
+            </div>
         </div>
     </div>
-</section>
 @endsection
